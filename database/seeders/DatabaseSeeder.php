b6ed59b6e09c4edeb8f9589c32e77b61d52531e5
GalihTitis <hhonggil007@gmail.com>
2022-04-23 11:50:42 +0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,7 +6,9 @@ use App\Models\Kategori;
 use App\Models\KategoriPost;
 use App\Models\Post;
 use App\Models\PostKategori;
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,9 +20,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
-        Post::factory(10)->create();
-        Kategori::factory(3)->create();
-        KategoriPost::factory(10)->create();
+        // \App\Models\User::factory(10)->create();
+        User::create([
+            'name'      => 'user',
+            'email'     => 'user@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make(12345678),
+            'role'      => 'user'
+        ]);
+        User::create([
+            'name'      => 'admin',
+            'email'     => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make(12345678),
+            'role'      => 'admin'
+        ]);
+        Kategori::create([
+            'nama'      => 'Web Programming',
+            'slug'      => 'web-programming',
+        ]);
+        Kategori::create([
+            'nama'      => 'Tutorial',
+            'slug'      => 'tutorial',
+        ]);
+        // Post::factory(10)->create();
+        // Kategori::factory(3)->create();
+        // KategoriPost::factory(10)->create();
     }
 }
